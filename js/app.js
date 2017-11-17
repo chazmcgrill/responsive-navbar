dc640300d974730799938fca16444ffec81ce7c5
chazmcgrill <charlietaylordev@gmail.com>
2017-11-17 12:02:03 +0000

    Styling and Logic
    finished styling and added vanilla javascript logic for toggling visibility of menu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
-var hamburger = document.getElementsByClassName('hamburger')[0];
-var menu = document.getElementsByClassName('menu')[0];
-console.log(menu);
+var hamburger = document.querySelector('.hamburger');
+var menu = document.querySelector('.menu');
+var nav = document.querySelector('nav');
+
+window.addEventListener('resize', function(event) {
+  if (event.target.innerWidth >= 640) {
+    nav.style.display = 'block';
+  } else {
+    nav.style.display = 'none';
+    hamburger.classList.remove('is-active');
+  }
+});
 
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('is-active');
-  menu.classList.toggle('hidden');
+  nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
 }, false);
