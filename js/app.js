4417b88bd51f92bc5d59b8d7d4855b3173d6d2cf
chazmcgrill <charlietaylordev@gmail.com>
2017-11-18 14:32:09 +0000

    Fixes
    issues with mobile functionality changed the js style changes to a class toggle. Added a set initial width function

--- js/app.js
+++ js/app.js
@@ -2,16 +2,24 @@ var hamburger = document.querySelector('.hamburger');
 var menu = document.querySelector('.menu');
 var nav = document.querySelector('nav');
 
+function setWidth() {
+  if (window.innerWidth > 640) {
+    nav.classList.remove('hidden');
+  }
+}
+
 window.addEventListener('resize', function(event) {
   if (event.target.innerWidth >= 640) {
-    nav.style.display = 'block';
+    nav.classList.remove('hidden');
   } else {
-    nav.style.display = 'none';
+    nav.classList.add('hidden');
     hamburger.classList.remove('is-active');
   }
 });
 
 hamburger.addEventListener('click', function() {
   hamburger.classList.toggle('is-active');
-  nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+  nav.classList.toggle('hidden');
 }, false);
+
+setWidth()
